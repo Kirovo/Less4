29f7b9818e59cec30e98bd79656d74c0fe29aeb0
cash <cash@SKLAP363.cyber.local>
2021-09-17 16:43:24 +0200

    correction product-summary and form

--- FEapp/src/app/product-list/product/product.component.ts
+++ FEapp/src/app/product-list/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProductReceiverService } from '../../services/product-receiver.service';
 import { Product} from '../../models/Product'
 import { ProductShowService } from '../../services/product-show.service';
@@ -10,41 +10,42 @@ import { ProductShowService } from '../../services/product-show.service';
 })
 export class ProductComponent implements OnInit {
 	@Input() product:Product;
+  amountIdle:number;
 
   constructor(private cart: ProductReceiverService, private detail: ProductShowService) { 
-	this.product ={
-		url : '',
-		name : '',
-		price : 0,
-		description:'',
-		amount : 0
+	  this.product ={
+      url : '',
+      name : '',
+      price : 0,
+      description:'',
+      amount : 0
 		}
+    this.amountIdle=1;
+    
 	}
 
   ngOnInit(): void {
-	this.product.amount=1
+    this.product.amount=1
   }
-  addProducts(product:Product):void{
-    product.amount++
+  addProducts():void{
+    this.amountIdle++
   }
-  removeProducts(product:Product):void{
-    product.amount--
+  removeProducts():void{
+    this.amountIdle--
   }
   addToCart(){
-    this.cart.addToCart(this.product)
+    this.cart.addToCart(this.product, this.amountIdle)
   }
   getDetail(product:Product){
     this.detail.getDetail(product)
   }
 
   disabled():boolean{
-    if (this.product.amount == 1) {
+    if (this.amountIdle == 1) {
       return true
     } else {
       return false
     }
-
-
   }
 
 }
--- FEapp/src/app/services/product-receiver.service.ts
+++ FEapp/src/app/services/product-receiver.service.ts
@@ -16,15 +16,20 @@ export class ProductReceiverService {
     this.fullName="";
   }
   
-  addToCart(product: Product){
-    try{
-      product.id=this.id
-      this.subOrder.push(product)
-      this.id++;
-      alert(product.amount+" "+product.name+" have been added to the cart")
-    }catch{
-      alert("Something went wrong. Please try again later")
-    }
+  addToCart(product: Product, amount:number) {
+      let found = this.subOrder.find(ele => ele.name == product.name)
+      let index = this.subOrder.findIndex(ele => ele.name == product.name)
+      if (typeof found === 'undefined') {
+        product.id=this.id
+        product.amount=amount
+        this.subOrder.push(product);
+        this.id++;
+      } else {
+        this.subOrder[index].amount=this.subOrder[index].amount+amount;
+      }
+      
+      alert(amount+" "+product.name+" have been added to the cart")
+
   }
 
   resetCart(){
@@ -33,6 +38,7 @@ export class ProductReceiverService {
   deleteItem(product:Product){
     const item = this.subOrder.findIndex(ele => ele.id == product.id)
     this.subOrder.splice(item,1)
+    alert(product.amount + " " + product.name + " have been removed" )
   }
   getName(fullName:string){
     this.fullName=fullName
