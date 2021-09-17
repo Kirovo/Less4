33b61774c1ee819a86a65f1a817d82850c09f035
cash <cash@SKLAP363.cyber.local>
2021-09-17 17:26:08 +0200

    correction product-summary and form

--- FEapp/src/app/cart/checkout/checkout.component.ts
+++ FEapp/src/app/cart/checkout/checkout.component.ts
@@ -9,7 +9,9 @@ export class CheckoutComponent implements OnInit {
   fullName:string=""
   address:string=""
   cardNum:string=""
-
+  good:boolean=false
+  good2:boolean=false
+  good3:boolean=false
 
 
   constructor(private getter: ProductReceiverService) { }
@@ -22,6 +24,24 @@ export class CheckoutComponent implements OnInit {
     this.cardNum='';
     this.address='';
   }
-
+  untilGood(arg:number){
+    if(this.fullName.length >= 8)
+    this.good=true
+    else
+    this.good=false
+  }
+  untilGood2(arg:number){
+    if(this.address.length >= 4)
+    this.good2=true
+    else
+    this.good2=false
+  }
+  untilGood3(arg:number){
+    console.log(typeof this.cardNum)
+    if(this.cardNum.length == 16 && typeof parseInt(this.cardNum) ==='number')
+    this.good3=true
+    else
+    this.good3=false
+  }
 
 }
